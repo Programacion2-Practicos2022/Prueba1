a527afae4bb904a18a09e959af7d49d7091c336d
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2022-02-14 18:44:23 +0000

    Corregí nombre de la clase Main

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void Principal(String[] args) {
+	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
 		
-		System.out.println("Hello Programación2 ");
+		System.out.println("Hello Programacion2 ");
 
   }
 }
