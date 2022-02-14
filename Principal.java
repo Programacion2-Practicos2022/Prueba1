a4e79e3e86770572051969e4bb06d89d38c955f4
eliizquierdo <eizquierdo@docente.ceibal.edu.uy>
2022-02-14 14:33:41 -0300

    Primer commit

---
+++ Principal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void Principal(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Hello Programación2 ");
+
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+## Ejemplo1
+
+Agregue las líneas necesarias al archivo **Principal.java** para que al ejecutar diga:
+**Hola profe, mi nombre es "SU NOMBRE Y APELLIDO!!**
