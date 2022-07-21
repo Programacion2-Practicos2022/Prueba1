167844984b895b81873ef70037ae049850147f1b
eliizquierdo <eizquierdo@docente.ceibal.edu.uy>
2022-07-21 18:47:57 -0300

    Subido desde eclipse

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
 		
-		System.out.println("Hola Programacion2 Git ");
+		System.out.println("Hola Programacion2 Git desde Eclipse ");
 
   }
 }
